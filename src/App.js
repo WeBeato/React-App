84dfb94cf9c0f45c9dec33e0d0f46157be4d719a
webeato <webeato.ad@gmail.com>
2025-12-23 16:26:59 +0330

    show array

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // let name = 'Mohammad';
   const [name, setName] = useState('Mohammad');
+  const [events, setEvents] = useState([
+    { title: 'THOR: LOVE AND THUNDER (2022)', id: 1 },
+    { title: 'THE GRAY MAN (2022)', id: 2 },
+    { title: 'THE SEA BEST (2022)', id: 3 },
+    { title: 'TOP GUN: MAVERICK (2022)', id: 4 },
+    { title: 'DOCTOR STRANGE IN THE MULTIVERSE OF MADNESS (2022)', id: 5 },
+    { title: 'HUSTLE (2022)', id: 6 }
+  ]);
 
   const handleClick = () => {
-    // name = 'Webeato';
-
     setName('Webeato');
     console.log(name);
   }
@@ -16,6 +21,13 @@ function App() {
     <div className="App">
       <h1>{name}</h1>
       <button onClick={handleClick}>Change Name</button>
+      {
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>{index + 1}- {event.title}</h2>
+          </div>
+        ))
+      }
     </div>
   );
 }
