b399d4f76eb39fba96beddb409f642a38ab5a383
webeato <webeato.ad@gmail.com>
2025-12-30 13:39:29 +0330

    add title component

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+export default function Title() {
+    return (
+        <div>
+            <h1 className="title">Best 2022 movies</h1>
+            <br />
+            <h3 className="subtitle">Latest movies</h3>
+        </div>
+    )
+}
