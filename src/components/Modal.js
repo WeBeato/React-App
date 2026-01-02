156f3f5e8d6d225129db4a6d0c766a75aa387923
webeato <webeato.ad@gmail.com>
2026-01-03 00:10:11 +0330

    use inner style

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,10 @@ import './Modal.css';
 export default function Modal(props) {
     return (
         <div className="modal-backdrop">
-            <div className="modal">
+            <div className="modal" style={{
+                border: "4px solid",
+                borderColor: "#ff4500"
+            }}>
                 {props.children}
                 <button onClick={props.handleClose}>Close</button>
             </div>
