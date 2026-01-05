2152c89187914e6812e3dfab44e156f045b9ec22
webeato <webeato.ad@gmail.com>
2026-01-05 11:13:01 +0330

    add reset form

--- src/components/NewMovieForm.js
+++ src/components/NewMovieForm.js
@@ -5,20 +5,34 @@ export default function NewMovieForm() {
     const [title, setTitle] = useState("");
     const [date, setDate] = useState("");
 
+    const resetForm = () => {
+        setTitle("");
+        setDate("");
+    }
+
 
     return (
         <form className='new-event-form'>
             <label>
                 <span>Movie Title:</span>
-                <input type="text" onChange={(e) => setTitle(e.target.value)} />
+                <input
+                    type="text"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
             </label>
             <label>
                 <span>Movie Date:</span>
-                <input type="date" onChange={(e) => setDate(e.target.value)} />
+                <input
+                    type="date"
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                />
             </label>
             <button>Submit</button>
 
             <p>Title - {title}, Date - {date}</p>
+            <p onClick={resetForm}>Reset Form</p>
         </form>
     )
 }
